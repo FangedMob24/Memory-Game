eada012812497e2fc9859f2eb4c6a729d68989e3
Hunter Powell <hunterp1510@gmail.com>
2022-09-03 14:30:44 -0500

    adding jasmine testing library

---
+++ memory.test.js
@@ -0,0 +1,5 @@
+//it(...) is a function that accepts two arguments
+//the first argument is the name used to refer to the test
+//the second argument is a callback function that executes the test
+
+it('should cal the high bracket',function(){})
